286b2d63e5b2bf2b1c18da1c3ec799913efc714a
Wadhah Mahrouk <wadhah.mahroug15@gmail.com>
2020-12-21 11:32:00 +0100

    refactor

--- src/main.py
+++ src/main.py
@@ -4,19 +4,24 @@ from bs4 import BeautifulSoup
 
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
-timeout = time.time() + 75
 
-with open("../data.txt", 'r') as dataFile:
-    data = dataFile.read()
 
-soup = BeautifulSoup(data, 'html.parser')
+def write_text(timeout, pause, interval):
+    with open("../data.txt", 'r') as dataFile:
+        data = dataFile.read()
 
-for i in range(10):
-    print("{}/10".format(i + 1))
-    time.sleep(1)
+    soup = BeautifulSoup(data, 'html.parser')
 
-for i in soup.find_all('span'):
-    pyautogui.typewrite(i.text + " ", pause=0.3, interval=0)
-    print(i.text)
-    if time.time() > timeout:
-        break
+    for i in range(10):
+        print("{}/10".format(i + 1))
+        time.sleep(1)
+
+    for i in soup.find_all('span'):
+        pyautogui.typewrite(i.text + " ", pause=pause, interval=interval)
+        print(i.text)
+        if time.time() > timeout:
+            break
+
+
+if __name__ == "__main__":
+    write_text(time.time() + 75, 0.3, 0)
